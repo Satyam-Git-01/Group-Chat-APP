07c857b24d4f427f0ccf68dc49ddb570b0eac790
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-05-17 13:53:42 +0530

    Encryption Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const path = require("node:path");
+const bcrypt = require("bcrypt");
 const UserModel = require("../models/userModel");
 const getSignUpPage = (req, res, next) => {
   res.sendFile(path.join(__dirname, "../", "public", "views", "signup.html"));
@@ -8,13 +9,13 @@ const handleLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await UserModel.findOne({
-      where: { email: email, password: password },
+      where: { email: email},
     });
-    console.log(user);
-    if (user) {
+    const result = await bcrypt.compare(password, user.password);
+    if (result) {
       res.status(200).json({ success: true, message: "Login Successfull" });
     } else {
-      res.status(400).json({ success: false, message: "Credentials Error" });
+      res.status(400).json({ success: false, message: "Credentials Error Enter Correct Data!" });
     }
   } catch (err) {
     res.status(500).json({ success: false, message: "Login Failed" });
@@ -24,12 +25,14 @@ const handleLogin = async (req, res, next) => {
 const handleSignUp = async (req, res, next) => {
   try {
     const { name, email, phone, password } = req.body;
-    await UserModel.create({ name, email, phone, password });
+    const encryptedpassword = await bcrypt.hash(password, 10);
+    //console.log(encryptedpassword)
+    await UserModel.create({ name, email, phone, password: encryptedpassword });
     res
       .status(201)
       .json({ success: true, message: "user create successfully" });
   } catch (err) {
-    res.status(500).json({ success: false, message: "user creation failed" });
+    res.status(500).json({ success: false, message: "Email already Exists" });
   }
 };
 module.exports = {
